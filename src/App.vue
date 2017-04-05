0284f45593a91e4b85a9c839c92eabd760302b04
pluhila <p2rlin.luhila@gmail.com>
2017-04-05 22:24:43 +0300

    Initial gameplay

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <div id="wheel-wrapper"></div>
-    <div id="fortune-wheel"></div>
-
+    <div id="prediction-wrapper">
+        <div class="prediction-title">Ennusta, milline värv õnnestub sul välja keerutada:</div>
+        <div class="prediction" v-for="color in colors" v-on:click="prediction = color.hex" v-bind:style="{backgroundColor: color.hex}" v-bind:class="{ active: prediction === color.hex }"></div>
+    </div>
+      <div id="arrow-wrapper">
+          <div id="arrow"></div>
+          <div id="fortune-wheel"></div>
+    </div>
     <button v-on:click="spin()">Keeruta</button>
   </div>
 </template>
@@ -12,6 +17,21 @@ export default {
   name: 'app',
   data () {
     return {
+      colors: [
+          { "hex": "#8fc31f", "name": "light green" },
+          { "hex": "#ffff00", "name": "yellow" },
+          { "hex": "#f39800", "name": "orange" },
+          { "hex": "#e60012", "name": "red" },
+          { "hex": "#e5004f", "name": "dark pink" },
+          { "hex": "#e4007f", "name": "pink" },
+          { "hex": "#920783", "name": "violet" },
+          { "hex": "#1d2088", "name": "dark blue" },
+          { "hex": "#0068b7", "name": "blue" },
+          { "hex": "#00a0e9", "name": "light blue" },
+          { "hex": "#009e86", "name": "green" },
+          { "hex": "#009944", "name": "dark green"}
+      ],
+      prediction: "#8fc31f",
       angle: 15
     }
   },
@@ -24,7 +44,17 @@ export default {
           this.angle = this.angle + (30 * rand);
 
           let wheel = document.getElementById("fortune-wheel");
-          wheel.style.transform = "rotate(" + this.angle + "deg)";
+          wheel.style.transform = "rotate(-" + this.angle + "deg)";
+
+          let color_index = (((this.angle - 15) / 30) % 12) + 1;
+          if (color_index === 12) color_index = 0;
+          let color = this.colors[color_index];
+          console.log(color["name"]);
+          if (color["hex"] === this.prediction) {
+              console.log("Wohoo!!! Võitsid miljon eurot!");
+          } else {
+              console.log("Böööö! Kaotasid!");
+          }
       }
     }
 }
@@ -32,44 +62,105 @@ export default {
 
 <style lang="scss">
   * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    vertical-align: top;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      vertical-align: top;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      color: #5d5d5d;
   }
 
   body, html {
-    height: 100%;
+      height: 100%;
   }
 
-  #wheel-wrapper {
-    width: 600px;
-    height: 811px;
-    background: url("./assets/wheel_wrapper.png") no-repeat center center;
-    background-size: contain;
-    position: absolute;
-    bottom: 50px;
-    left: 50%;
-    margin-left: -300px;
-    z-index: 100;
+  body {
+      overflow-y: scroll;
+  }
+
+  #app {
+      width: 620px;
+      margin: 0 auto;
+  }
+
+  #arrow-wrapper {
+      width: 570px;
+      height: 570px;
+      margin: 0 auto;
+      background: #5d5d5d;
+      border-radius: 50%;
+      padding: 10px;
+      position: relative;
+  }
+
+  #arrow {
+      width: 0;
+      height: 0;
+      border-left: 20px solid transparent;
+      border-right: 20px solid transparent;
+      border-top: 20px solid #5d5d5d;
+      position: absolute;
+      left: 50%;
+      margin-left: -20px;
+      z-index: 10;
   }
 
   #fortune-wheel {
     width: 550px;
     height: 550px;
+    margin: 0 auto;
     background: #cccccc;
-    position: absolute;
     border-radius: 50%;
     background: url("./assets/wheel2.png") no-repeat center center;
     background-size: contain;
-    z-index: 10;
-    bottom: 280px;
-    left: 50%;
-    margin-left: -275px;
-    cursor: pointer;
-    transition-duration: 1s;
+    transition-duration: 3s;
     transition-property: transform;
-    transition-timing-function: ease-in-out;
+    transition-timing-function: ease-out;
     transform: rotate(15deg);
+  }
+
+  #prediction-wrapper {
+      padding: 25px 10px;
+      font-weight: bold;
+  }
+
+  .prediction-title {
+      margin: 15px 0;
+      text-align: center;
+      font-size: 20px;
+  }
+
+  .prediction {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin: 5px;
+      display: inline-block;
+      cursor: pointer;
+      border: 5px solid #fff;
+  }
+
+  .prediction:hover, .prediction.active {
+      border: none;
+  }
+
+  button {
+      border: none;
+      width: 100%;
+      height: 40px;
+      margin: 40px 0 15px 0;
+      font-size: 16px;
+      color: #fff;
+      cursor: pointer;
+      font-weight: bold;
+      background: #00a0e9;
+  }
+
+  button:hover {
+      background: #008dd3;
+  }
+
+  button:focus {
+      outline: none;
   }
 </style>
